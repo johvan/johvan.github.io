52f268eea3c0df98bc20f2f775ea9f8108801ae4
Johvan <johvanwyke@gmail.com>
2017-09-06 09:00:32 -0400

    Changes made here

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ this.addEventListener("install", function(event) {
         caches.open("v1").then(function(cache) {
             // origin-relative urls; Add the resources you want to cache.
             return cache.addAll([
-                "src",
+                "https://johvan.github.io/src",
                 "index.html",
                 "style.css",
                 "src/app.js",
